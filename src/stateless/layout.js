3a6373cb297374a7c09ad1bd36938b64378e7944
karthiks <sirkarthik@gmail.com>
2020-04-04 22:05:58 +0530

    Testing CSS Module inbuilt integration from create-react-app

--- src/stateless/layout.js
+++ src/stateless/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../hoc/aux'
+import classes from './layout.module.css';
 
 const Layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
