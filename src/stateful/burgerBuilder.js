b025c17d3646b1344371243825893c730aa54026
karthiks <sirkarthik@gmail.com>
2020-04-05 01:22:57 +0530

    Added state in BurgerBuilder to pass ingredients to Burger for dynamic ingredients

--- src/stateful/burgerBuilder.js
+++ src/stateful/burgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../hoc/aux';
 import Burger from '../stateless/burger'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 3,
+            bacon: 1,
+            cheese: 2,
+            meat:1,
+        }
+    };
+
     render() {
         return (
             <Aux>
-                <Burger></Burger>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Burger Controls</div>
             </Aux>
         );
--- src/stateless/burger.js
+++ src/stateless/burger.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import classes from './burger.module.css'
+import PropTypes from 'prop-types';
+import classes from './burger.module.css';
 import BurgerIngredient from './burgerIngredient';
 
-const Burger = props => {
+const Burger = (props) => {
+    const ingredientsList = Object.keys(props.ingredients);
+    
+    const transformedIngredients = ingredientsList.map(ing => {
+        const ing_count = props.ingredients[ing];
+        return [...Array(ing_count)].map( (_,index) => {
+            return (<BurgerIngredient type={ing} key={ing + index} />);
+        });
+    });
+
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="salad" />
-            <BurgerIngredient type="bacon" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
+}
+
+Burger.propTypes = {
+    ingredients: PropTypes.shape({
+        "bread-top" : PropTypes.number,
+        "meat" : PropTypes.number,
+        "cheese" : PropTypes.number,
+        "salad" : PropTypes.number,
+        "bacon" : PropTypes.number,
+        "bread-bottom" : PropTypes.number
+    })
 }
 
 export default Burger;
