670668efd3fabebf0daa0e5c9a8224e7bccd6665
karthiks <sirkarthik@gmail.com>
2020-04-06 01:05:25 +0530

    Added Order Button and PropTypes validation for ingredientsMenu component

--- src/stateless/ingredientsMenu.js
+++ src/stateless/ingredientsMenu.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import IngredientsMenuItem from './ingredientsMenuItem'
+import React from 'react';
+import IngredientsMenuItem from './ingredientsMenuItem';
 import classes from './ingredientsMenu.module.css';
+import PropTypes from 'prop-types';
 
 const controls = [
     { label: 'Salad', type: 'salad' },
@@ -10,6 +11,10 @@ const controls = [
 ];
 
 const IngredientsMenu = (props) => {
+    const inglst = props.ingredients;
+    const ingredientsAddedCount = Object.keys(inglst)
+        .map(k => inglst[k])
+        .reduce((sum,v)=>sum+v,0);
     return (
         <div className={classes.BuildControls}>
             <p>Price Now is <strong>{props.price}</strong></p>
@@ -27,8 +32,32 @@ const IngredientsMenu = (props) => {
                 )
             })
         }
+            <button 
+                className={classes.OrderButton}
+                disabled={ingredientsAddedCount<1}
+            > Order Now </button>
         </div>
     );
 };
+
+IngredientsMenu.propTypes = {
+    ingredients: function (props, propName, componentName) {
+        const ingredients = props[propName];
+        const ingredientsList = Object.keys(ingredients);
+        if (typeof (ingredients) !== typeof ({})) {
+            return new Error(`Invalid prop "${propName}" supplied to "${componentName}". Validation Failed!`);
+        }
+        const predefinedIngredients = ["salad", "bacon", "cheese", "meat"];
+        const hasUnknownIngredients = ing => !predefinedIngredients.includes(ing);
+        const undefinedIngredients = ingredientsList.filter(k => hasUnknownIngredients(k));
+        if (undefinedIngredients.length > 0) {
+            return new Error(`Invalid prop "${propName}" supplied to "${componentName}". 
+            Validation Failed because of unknown ingredients like "${undefinedIngredients}"`);
+        }
+    },
+    price: PropTypes.number.isRequired,
+    addIngredient: PropTypes.func.isRequired,
+    removeIngredient: PropTypes.func.isRequired
+}
 
 export default IngredientsMenu;
