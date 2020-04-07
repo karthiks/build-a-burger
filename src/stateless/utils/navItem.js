b13ef8b1f864f0c1fdef0a21742fbe299334f4bf
karthiks <sirkarthik@gmail.com>
2020-04-07 14:05:26 +0530

    Removed console.log stmt

--- src/stateless/utils/navItem.js
+++ src/stateless/utils/navItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './navItem.module.css';
 
 const NavItem = (props) => {
-    console.log(props.active);
     return (
         <li className={classes.NavItem}>
             <a to={props.link} className={props.active ? classes.active : null}>
