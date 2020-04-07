35476548418da28b7db20dcb1408ebea89bb83e9
karthiks <sirkarthik@gmail.com>
2020-04-08 00:18:15 +0530

    Performance Tuning: When you don't want to compute order lists in Modal, return null

--- src/stateless/utils/modal.js
+++ src/stateless/utils/modal.js
@@ -3,15 +3,24 @@ import classes from "./modal.module.css";
 import Aux from '../../hoc/aux';
 import Backdrop from './backdrop';
 import PropTypes from 'prop-types';
+import { render } from '@testing-library/react';
 
-const Modal = (props) => (
-    <Aux>
-        <Backdrop show={props.orderNow} cancel={props.cancelOrder} />
-        <div className={classes.Modal} style={{ ...props.style }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+const Modal = (props) => {
+    // console.log("Show Modal? : ", props.orderNow);
+    if (!props.orderNow) { // Why render complex lists when not required?
+        // console.log("When Model doesn't render a thing!!..")
+        return null; // Returning null to render nothing!!.. 
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.orderNow} cancel={props.cancelOrder} />
+            <div className={classes.Modal} style={{ ...props.style }}>
+                {props.children}
+            </div>
+        </Aux>
+    );
+};
 
 Modal.propTypes = {
     orderNow: PropTypes.bool.isRequired,
