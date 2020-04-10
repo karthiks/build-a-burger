2d7687b5dcc4e60f4399ca4fc975abed596cbcb3
karthiks <sirkarthik@gmail.com>
2020-04-11 00:38:14 +0530

    Applying custom theme to storybook

---
+++ .storybook/myTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+
+    brandTitle: 'Cody Burger Storybook',
+    brandImage: 'http://codonomics.com/favicon.ico',
+    brandUrl: 'https://codonomics.com',
+});
