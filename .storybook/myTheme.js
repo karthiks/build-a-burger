31a89fe93fd33f8f0be8cb010ba27edc17b1c844
karthiks <sirkarthik@gmail.com>
2020-04-11 00:39:41 +0530

    Storybook Custom Theme: Fixing invisible brandTitle when brandImage is present

--- .storybook/myTheme.js
+++ .storybook/myTheme.js
@@ -3,7 +3,8 @@ import { create } from '@storybook/theming/create';
 export default create({
     base: 'dark',
 
-    brandTitle: 'Cody Burger Storybook',
-    brandImage: 'http://codonomics.com/favicon.ico',
+    // brandTitle: 'Cody Burger Storybook',
+    // brandImage: 'http://codonomics.com/favicon.ico',
+    brandTitle: `<img src='http://codonomics.com/favicon.ico'>Cody Burger Storybook</img>`,
     brandUrl: 'https://codonomics.com',
 });
